59f4a53d620a4ff2215aa7ebddcf3ba0b4c08a62
azan <cornaple@gmail.com>
2024-11-15 02:11:32 +0300

    chore(mappings): Bring `set_buf_keymap` to the same style

--- lua/lua-console/mappings.lua
+++ lua/lua-console/mappings.lua
@@ -8,88 +8,94 @@ M.set_buf_keymap = function()
   --- @type number
   local buf = Lua_console.buf
 
-  vim.api.nvim_buf_set_keymap(buf, "n", mappings.resize_up, "", {
-    desc = "Resize up",
+  vim.api.nvim_buf_set_keymap(buf, 'n', mappings.resize_up, '', {
+    desc = 'Resize up',
     callback = function()
       vim.cmd.resize('+5')
-    end
+    end,
   })
 
-  vim.api.nvim_buf_set_keymap(buf, "n", mappings.resize_down, "", {
-    desc = "Resize down",
+  vim.api.nvim_buf_set_keymap(buf, 'n', mappings.resize_down, '', {
+    desc = 'Resize down',
     callback = function()
       vim.cmd.resize('-5')
-    end
+    end,
   })
 
-  vim.api.nvim_buf_set_keymap(buf, "n", mappings.clear, "", {
+  vim.api.nvim_buf_set_keymap(buf, 'n', mappings.clear, '', {
     desc = 'Clear console',
     callback = function()
-      vim.api.nvim_buf_set_lines(0, 1, -1, false, {''})
-    end
+      vim.api.nvim_buf_set_lines(0, 1, -1, false, { '' })
+    end,
   })
 
-  vim.api.nvim_buf_set_keymap(buf, "n", mappings.messages, "", {
+  vim.api.nvim_buf_set_keymap(buf, 'n', mappings.messages, '', {
     desc = 'Load messages',
-    callback = utils.load_messages
+    callback = utils.load_messages,
   })
 
-  vim.api.nvim_buf_set_keymap(buf, "n", "gf", "", {
-    desc = "Open file in vertical split",
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'gf', '', {
+    desc = 'Open file in vertical split',
     callback = function()
-      local file = vim.fn.expand "<cfile>"
+      local file = vim.fn.expand('<cfile>')
       local line = utils.get_source_lnum()
 
       vim.api.nvim_win_close(0, false)
 
-      vim.cmd("vs " .. file)
+      vim.cmd('vs ' .. file)
+  -- stylua: ignore
       if line then vim.api.nvim_win_set_cursor(0, {line, 0}) end
     end,
   })
 
-  vim.keymap.set({"n", "v"}, mappings.eval, "", {
-    buffer = buf,
-    desc = "Eval lua code in current line or visual selection",
-    callback = utils.eval_lua_in_buffer
+  vim.api.nvim_buf_set_keymap(buf, 'n', mappings.eval, '', {
+    desc = 'Eval lua code in current line or visual selection',
+    callback = utils.eval_lua_in_buffer,
+    noremap = true,
+  })
+  vim.api.nvim_buf_set_keymap(buf, 'v', mappings.eval, '', {
+    desc = 'Eval lua code in current line or visual selection',
+    callback = utils.eval_lua_in_buffer,
+    noremap = true,
   })
 
-  vim.keymap.set({"n"}, mappings.save, "", {
-    buffer = buf,
-    desc = "Save console",
+  vim.api.nvim_buf_set_keymap(buf, 'n', mappings.save, '', {
+    desc = 'Save console',
     callback = function()
       vim.cmd('write! ' .. config.buffer.save_path)
-    end
+    end,
+    noremap = true,
   })
 
-  vim.keymap.set({"n"}, mappings.load, "", {
-    buffer = buf,
-    desc = "Load console",
-    callback = utils.load_console
+  vim.api.nvim_buf_set_keymap(buf, 'n', mappings.load, '', {
+    desc = 'Load console',
+    callback = utils.load_console,
   })
 
-  vim.keymap.set({"n"}, mappings.quit, "", {
-    buffer = buf,
-    desc = "Close console",
+  vim.api.nvim_buf_set_keymap(buf, 'n', mappings.quit, '', {
+    desc = 'Close console',
     callback = function()
       vim.api.nvim_win_close(0, false)
-    end
+    end,
+    noremap = true,
   })
 
-  vim.keymap.set({"n"}, 'g?', "", {
-    buffer = buf,
-    desc = "Toggle help",
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'g?', '', {
+    desc = 'Toggle help',
     callback = function()
       utils.toggle_help()
-    end
+    end,
+    noremap = true,
   })
 end
 
 M.set_buf_autocommands = function()
-  vim.api.nvim_create_autocmd({ "BufLeave", "BufWinLeave" }, {
+  vim.api.nvim_create_autocmd({ 'BufLeave', 'BufWinLeave' }, {
     buffer = Lua_console.buf,
-    desc = "Close window when focus is lost",
+    desc = 'Close window when focus is lost',
     callback = function()
       local win = Lua_console.win
+  -- stylua: ignore
       if not win then return end
 
       if vim.api.nvim_win_is_valid(win) then
@@ -98,7 +104,7 @@ M.set_buf_autocommands = function()
       end
 
       Lua_console.win = false
-    end
+    end,
   })
 end
 
